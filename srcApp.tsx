d6c9a5143446e3219c130f58692d8f472d597a41
Ahmed Backer <ahmedbackerjr@gmail.com>
2025-11-16 14:00:21 +0000

    Remove orphaned duplicate files from root directory

--- srcApp.tsx
+++
@@ -1,8 +0,0 @@
-import "./components/Website.css";   // <-- all styles live here
-import { WebsiteRouter } from "./components/WebsiteRouter";
-
-function App() {
-  return <WebsiteRouter />;
-}
-
-export default App;
